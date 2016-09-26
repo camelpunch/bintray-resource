8282dca1f45f959aa599e50eba03e6a3c4aabee6
Andrew Bruce <me@andrewbruce.net>
2016-09-26 20:48:20 +0100

    Remove test setup in favour of fixture methods

--- test/fast/bintray_resource/out_test.rb
+++ test/fast/bintray_resource/out_test.rb
@@ -8,30 +8,6 @@ require_relative '../../doubles/reader_stub'
 
 module BintrayResource
   class TestOut < Minitest::Test
-    def setup
-      @input = {
-        "source" => {
-          "api_key"         => "abcde123456",
-          "package"         => "rabbitmq_clusterer",
-          "repo"            => "community-plugins",
-          "subject"         => "rabbitmq",
-          "username"        => "myuser",
-        },
-        "params" => {
-          "file"            => "my-source/built-*.ez",
-          "publish"         => true,
-          "version_regexp"  => "my-source/(.*)/built-.*",
-          "licenses"        => ["Mozilla-1.1"],
-          "vcs_url"         => "https://github.com/rabbitmq/rabbitmq-clusterer",
-        },
-      }
-      @input_with_list = @input.merge(
-        "params" => @input["params"].merge(
-          "list_in_downloads" => true
-        )
-      )
-    end
-
     def test_create_and_upload
       reader = ReaderStub.new(
         stub: { glob: "/sources/path/my-source/built-*.ez", regexp: "my-source/(.*)/built-.*", },
@@ -42,7 +18,7 @@ module BintrayResource
       upload = UploadSpy.new([201, 200], "")
       resource = Out.new(reader: reader, upload: upload)
 
-      resource.call("/sources/path", @input)
+      resource.call("/sources/path", generic_input)
 
       expected_uris = %w(
         https://myuser:abcde123456@bintray.com/api/v1/packages/rabbitmq/community-plugins
@@ -71,7 +47,7 @@ module BintrayResource
       upload = UploadSpy.new([201, 200, 200], "")
       resource = Out.new(reader: reader, upload: upload)
 
-      resource.call("/sources/path", @input_with_list)
+      resource.call("/sources/path", generic_input_with_list)
 
       assert_equal(
         "https://myuser:abcde123456@bintray.com/api/v1/file_metadata/rabbitmq/community-plugins/built-package12345.ez",
@@ -90,7 +66,7 @@ module BintrayResource
     def test_emits_version_passed_to_it
       reader = ReaderStub.new(to_return: Reader::Response.new("", "1.2.3", ""))
       retval = Out.new(reader: reader, upload: UploadSpy.new([201, 200])).
-        call("/full/sources", @input)
+        call("/full/sources", generic_input)
       assert_equal({ "ref" => "1.2.3" }, retval["version"])
     end
 
@@ -99,7 +75,7 @@ module BintrayResource
         reader: ReaderStub.new,
         upload: UploadSpy.new([200, 200], '{"result":"success"}')
       )
-      retval = resource.call("/sources/path", @input)
+      retval = resource.call("/sources/path", generic_input)
 
       assert_equal(
         [
@@ -116,7 +92,7 @@ module BintrayResource
         upload: UploadSpy.new.ordered_failures(true)
       )
       assert_raises(BintrayResource::Upload::FailureResponse) do
-        resource.call("/sources/path", @input)
+        resource.call("/sources/path", generic_input)
       end
     end
 
@@ -126,8 +102,35 @@ module BintrayResource
         upload: UploadSpy.new([200, 200, 500]).ordered_failures(false, false, true)
       )
       assert_raises(BintrayResource::Upload::FailureResponse) do
-        resource.call("/sources/path", @input_with_list)
+        resource.call("/sources/path", generic_input_with_list)
       end
+    end
+
+    def generic_input
+      {
+        "source" => {
+          "api_key"         => "abcde123456",
+          "package"         => "rabbitmq_clusterer",
+          "repo"            => "community-plugins",
+          "subject"         => "rabbitmq",
+          "username"        => "myuser",
+        },
+        "params" => {
+          "file"            => "my-source/built-*.ez",
+          "publish"         => true,
+          "version_regexp"  => "my-source/(.*)/built-.*",
+          "licenses"        => ["Mozilla-1.1"],
+          "vcs_url"         => "https://github.com/rabbitmq/rabbitmq-clusterer",
+        },
+      }
+    end
+
+    def generic_input_with_list
+      generic_input.merge(
+        "params" => generic_input["params"].merge(
+          "list_in_downloads" => true
+        )
+      )
     end
   end
 end
